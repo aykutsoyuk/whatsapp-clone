cd6e10767eb3dbf025162e023298baf90db29d55
Aykut Soyuk <aykut@boomset.com>
2021-07-14 17:28:47 +0300

    initial commit

---
+++ src/db/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBcsZZ9qXcqjgRpII39xDnzF7xOUIEisHM",
+    authDomain: "whatsapp-clone-e6e0e.firebaseapp.com",
+    projectId: "whatsapp-clone-e6e0e",
+    storageBucket: "whatsapp-clone-e6e0e.appspot.com",
+    messagingSenderId: "97825297143",
+    appId: "1:97825297143:web:a048074dcc86dc9782f9f2"
+  };
