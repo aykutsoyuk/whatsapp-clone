1cffad1406ad9f9fd79d8a188b81eca3f9d518b5
Aykut Soyuk <aykut@boomset.com>
2021-07-17 10:24:47 +0300

    Messages page styles

--- src/views/Chat.js
+++ src/views/Chat.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/Chat.styles.css"
 
 const Chat = () => {
-  return <div className="main">chat</div>;
+  return <div className="main">chat<button className="messages-back-button">Back</button></div>;
 };
 
 export default Chat;
