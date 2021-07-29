51a20fd9e0f37d67c9861a8024a7d052fac7050c
Aykut Soyuk <aykut@boomset.com>
2021-07-29 18:24:08 +0300

    created portal for modal

--- src/components/UsersModal.js
+++ src/components/UsersModal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "../styles/UsersModal.styles.css";
 import { db } from "../db/index.js";
 
@@ -7,31 +8,50 @@ const UsersModal = (props) => {
     const uniqueID = Math.random().toString(36).substr(2, 9);
     const chatBody = {
       id: uniqueID,
-      members: [user?.id, props.currentUser?.uid]
+      members: [user?.id, props.currentUser?.uid],
     };
     await db.collection("chats").doc(chatBody.id).set(chatBody);
-    props.setIsModalOpen(false)
+    props.setIsModalOpen(false);
+  };
+
+  const ModalOverlay = (props) => {
+    return (
+      <div className="modalWrapper">
+        <div className="modal">
+          <h3>Create a chat</h3>
+          <button
+            className="closeButton"
+            onClick={() => props.setIsModalOpen(false)}
+          >
+            X
+          </button>
+          <ul className="chatList">
+            {props.users?.map(
+              (user) =>
+                user.email && (
+                  <li key={user.id} className="chatList-item">
+                    <button onClick={() => createChat(user)}>
+                      {user.email}
+                    </button>
+                  </li>
+                )
+            )}
+          </ul>
+        </div>
+      </div>
+    );
   };
 
   return (
-    <div className="modalWrapper">
-      <div className="modal">
-        <h3>Create a chat</h3>
-        <button
-          className="closeButton"
-          onClick={() => props.setIsModalOpen(false)}
-        >
-          X
-        </button>
-        <ul className="chatList">
-          {props.users?.map((user) => (user.email &&
-            <li key={user.id} className="chatList-item">
-              <button onClick={() => createChat(user)}>{user.email}</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          setIsModalOpen={props.setIsModalOpen}
+          users={props.users}
+        />,
+        document.getElementById("modal-overlay")
+      )}
+    </React.Fragment>
   );
 };
 
